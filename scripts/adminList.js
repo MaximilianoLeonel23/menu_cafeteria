2ae6a9cd0dc789eabe4ff602fdd5b8d9bacf64d4
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-04-05 18:45:04 -0300

    Se agregan ultimas funcionalidades de listar productos. Se agregan imágenes a los headers de cada section y ultiman detalles.

--- scripts/adminList.js
+++ scripts/adminList.js
@@ -1,12 +1,8 @@
 import { products } from "./products.js";
+
 const adminProductList = document.getElementById("adminProductList");
 
 export const listProducts = () => {
-  for (let i = 0; i < products.length; i++) {
-    while (adminProductList[i].firstChild) {
-      adminProductList[i].removeChild(adminProductList[i].firstChild);
-    }
-  }
   products.map((el) => {
     const listItem = createListItem(el);
     adminProductList.appendChild(listItem);
